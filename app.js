acfefe45093cdb7fe4c266cb1c6a049b15a183a2
san2ride <sanchezjason@me.com>
2018-08-05 17:02:29 -0600

    True and False Values II

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-let wordCount = true;
-
-if (wordCount) {
-  console.log("Great! You've started your work!");
-} else {
-  console.log('Better get to work!');
-}
-
-
 let favoritePhrase = true;
 
-if (favoritePhrase) {
+if (!favoritePhrase) {
   console.log("This string doesn't seem to be empty.");
 } else {
   console.log('This string is definitely empty.');
