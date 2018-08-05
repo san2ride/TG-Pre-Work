06630c1ee762dfbce814bd0ea7cd6bd1d1cb8d2d
san2ride <sanchezjason@me.com>
2018-08-05 17:32:16 -0600

    else if Statements

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var moonPhase = "full"
-if (moonPhase === "full") {
-  console.log("Howl!")
+let moonPhase = 'solar eclipse';
+
+if (moonPhase === 'full') {
+  console.log('Howl!');
+} else if (moonPhase === 'mostly full') {
+  console.log('Arms and legs are getting hairier');
+} else if (moonPhase === 'mostly new') {
+  console.log('Back on two feet');
 } else {
-  console.log("I swear I am not a werewolf.")
+  console.log('Invalid moon phase');
 }
