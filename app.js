84d3f5286742c0033f38efb880a8e68ba50c0293
san2ride <sanchezjason@me.com>
2018-08-05 17:41:41 -0600

    Ternary Operator

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-let moonPhase = 'full';
+let isLocked = false;
 
-switch (moonPhase) {
-  case 'full':
-    console.log('Howl!');
-    break;
-  case 'mostly full':
-    console.log('Arms and legs are getting hairier');
-    break;
-  case 'mostly new':
-    console.log('Back on two feet');
-    break;
-  default:
-    console.log('Invalid moon phase');
-    break;
-}
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
