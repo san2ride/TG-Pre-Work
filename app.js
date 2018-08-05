ebe473d227cf813559468358722b6f58938c4ac5
san2ride <sanchezjason@me.com>
2018-08-05 14:45:08 -0600

    Introduction to Control Flow

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-let myName = 'Nitin';
-let myCity = 'London';
+let userName = 'Jason';
+let knowsJavaScript = true;
 
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+if (knowsJavaScript && userName) {
+  console.log('Great, ' + userName + '! Get ready to practice your JavaScript!');
+} else if (knowsJavaScript) {
+  console.log('Great! Get ready to practice your JavaScript!');
+} else if (userName) {
+  console.log('Great, ' + userName + '! Get ready to learn something new!');
+} else {
+  console.log('Great! Get ready to learn something new!');
+}
