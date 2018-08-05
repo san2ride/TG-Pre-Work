51793c7b98e4b6484d06621c8993d30d7ba39f42
san2ride <sanchezjason@me.com>
2018-08-05 17:11:49 -0600

    Comparison Operators

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ if (hungerLevel > 7) {
 } else {
   console.log("We can eat later!")
 }
+
+let hungerLevel = 10
+if (hungerLevel > 7) {
+  console.log("Time to eat!")
+} else {
+  console.log("We can eat later!")
+}
