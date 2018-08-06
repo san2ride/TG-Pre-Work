5c4f0aad29335f60ff5e51491e21862ca3402d60
san2ride <sanchezjason@me.com>
2018-08-06 02:29:21 -0600

    Introduction to Classes

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
-// Review: Objects
-let person = {
-  _name: 'Lu Xun',
-  _age: 137,
-
-  set age(ageIn) {
-    if (typeof ageIn === 'number') {
-      this._age = ageIn;
-    }
-    else {
-      console.log('Invalid input');
-      return 'Invalid input';
-    }
-  },
-
-  get age() {
-    console.log(`${this._name} is ${this._age} years old.`);
-    return this._age;
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
   }
 
-};
+  get name() {
+    return this._name;
+  }
+  get behavior() {
+    return this._behavior;
+  }
 
-
-person.age = 'Thirty-nine';
-person.age = 39;
-
-console.log(person.age);
+  incrementBehavior() {
+    this._behavior ++;
+  }
+}
