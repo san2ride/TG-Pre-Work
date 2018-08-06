8574046400c8ac254356cc2bd1bcdbb86d09c4ef
san2ride <sanchezjason@me.com>
2018-08-05 22:39:37 -0600

    push Method

--- app.js
+++ app.js
@@ -6,3 +6,6 @@ console.log(listItem);
 console.log(newYearsResolutions[2]);
 console.log(newYearsResolutions[3]);
 console.log(newYearsResolutions.length)
+newYearsResolutions.push('item 3', 'item 4')
+console.log(newYearsResolutions)
+newYearsResolutions.pop([4])
