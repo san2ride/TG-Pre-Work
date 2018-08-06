77e17ecc191b5f95efa7265c856ec9c18ee069d9
san2ride <sanchezjason@me.com>
2018-08-06 02:01:34 -0600

    Review: Loops

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let flag = true;
-let counter = 0;
-while(flag === true){
-  console.log(counter);
-  counter+=1;
-  if (counter === 37){
-    break;
-  }
-}
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+
+groceryList.unshift('popcorn');
+console.log(groceryList);
+
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
