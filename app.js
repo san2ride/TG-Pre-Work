2d9be949e736b07196135c09fc7caa5f03485639
san2ride <sanchezjason@me.com>
2018-08-05 22:30:30 -0600

    Create an array

--- app.js
+++ app.js
@@ -1,3 +1,2 @@
-let bucketList = ['Rappel into a cave', 'Take a falconry class', 'Learn to juggle'];
-
-console.log(bucketList);
+var newYearsResolutions = ["", "", ""]
+console.log(newYearsResolutions)
