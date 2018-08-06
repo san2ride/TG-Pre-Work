dc787259b86b67ec41d30253a2d23c9d41315ed1
san2ride <sanchezjason@me.com>
2018-08-05 22:10:30 -0600

    Global Scope II

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const galaxy = 'The Milky Way'
 let stars = 'North Star'
 
 const myNightSky = () => {
+  stars = 'Sirius'
   return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy
 
 }
 
-console.log(myNightSky()) 
+console.log(myNightSky())
+console.log(stars)
