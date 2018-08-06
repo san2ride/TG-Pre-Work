4c485542b9a78effee28a5de77f4d449d48d1bf9
san2ride <sanchezjason@me.com>
2018-08-05 20:40:37 -0600

    Return

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const takeOrder = (topping) => {
-  console.log('Order: pizza topped with ' + topping);
+let orderCount = 0
+const takeOrder = (topping, crustType) => {
+  orderCount = orderCount + 1
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
 };
 
-takeOrder('mushrooms');
+const getSubTotal = (itemCount) => {
+  return (itemCount * 7.5)
+}
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
+console.log(getSubTotal(orderCount))
