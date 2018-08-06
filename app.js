964a895e43cc58aaaf56bf17c2f28d83341c8333
san2ride <sanchezjason@me.com>
2018-08-05 22:07:52 -0600

    Scope

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const volumeOfSphere = diameter => (1/6) * Math.PI * diameter * diameter * diameter;
+const satellite = 'The Moon'
+const galaxy = 'The Milky Way'
+let stars = 'North Star'
 
+const myNightSky = () => {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy
 
-console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
+}
+
+console.log(myNightSky()) 
