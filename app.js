9ce12feb189b2fe2d68ab0c567bdc9773e8afb1e
san2ride <sanchezjason@me.com>
2018-08-05 22:26:22 -0600

    Block Scope III

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const visibleLightWaves = () => {
-  let lightWaves = 'Moonlight';
-  let region = 'The Arctic';
-  if (region === 'The Arctic') {
-    let lightWaves = 'Northern Lights';
-    console.log(lightWaves);
-  }
-  console.log(lightWaves);
-};
+const starCount = () => {
+  let i = 5
+  console.log(i)
 
-visibleLightWaves();
+  for (let i = 0; i < 12; i++) {
+    console.log(i)
+  }
+}
+
+starCount()
+console.log(i)
