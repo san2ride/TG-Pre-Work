44e0e210bc9d7e2956c349a00e8a484a65f06220
san2ride <sanchezjason@me.com>
2018-08-05 22:33:23 -0600

    Property Access

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var newYearsResolutions = ["", "", ""]
-console.log(newYearsResolutions)
+let newYearsResolutions = ['Run a marathon', 'Learn a new language', 'Read 52 books'];
+
+let listItem = newYearsResolutions[0];
+console.log(listItem);
+
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
