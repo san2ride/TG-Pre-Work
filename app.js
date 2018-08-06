cf3662368f5c3c8768bb5a45bffcd20addc2fecb
san2ride <sanchezjason@me.com>
2018-08-06 03:13:21 -0600

    Named Imports

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-let availableAirplanes = [{
- name: 'AeroJet',
- fuelCapacity: 800,
- availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
-},
-{name: 'SkyJet',
- fuelCapacity: 500,
- availableStaff: ['pilots', 'flightAttendants']
-}];
+import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
 
-let flightRequirements = {
-  requiredStaff: 4,
-};
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
 
-function meetsStaffRequirements(availableStaff, requiredStaff) {
-  if (availableStaff.length >= requiredStaff) {
-    return true;
-  } else {
-    return false;
-  }
-};
+displayFuelCapacity();
 
-export { availableAirplanes, flightRequirements, meetsStaffRequirements};
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
+
+displayStaffStatus();
