3542d81b05df3dd2ac4419e631e44d3f7329396b
san2ride <sanchezjason@me.com>
2018-08-06 03:46:56 -0600

    Review Requests II

--- app.js
+++ app.js
@@ -1,31 +1,70 @@
-// information to reach API
-const apiKey = '<Your API Key>';
-const url = 'https://api.rebrandly.com/v1/links';
+// NOTE: wordSmith functions from lines 4 - 39
+// NOTE: byteSize functions from lines 48 - 81 (remember to add your API key!)
 
-// Some page elements
+// information to reach API
+const dataMuseUrl = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jjb=';
+
+// selecting page elements
 const inputField = document.querySelector('#input');
-const shortenButton = document.querySelector('#shorten');
+const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
+// AJAX function
+const getSuggestions = async () => {
+  const wordQuery = inputField.value;
+  const endpoint = dataMuseUrl + queryParams + wordQuery;
+
+  try{
+    const response =  await fetch(endpoint);
+    if(response.ok){
+      let jsonResponse = await response.json();
+			renderWordResponse(jsonResponse);
+    }
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+// clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
+
+// information to reach Rebrandly API
+const apiKey = '<Your API Key>';
+const rebrandlyEndpoint = 'https://api.rebrandly.com/v1/links';
+
+// element selector
+const shortenButton = document.querySelector('#shorten');
+
 // AJAX functions
-// Code goes here
-const shortenUrl = async () => {
-	const urlToShorten = inputField.value;
+const shortenUrl = async () =>{
+  const urlToShorten = inputField.value;
   const data = JSON.stringify({destination: urlToShorten});
-  try {
-    const response = await fetch(url, {
-			method: 'POST',
+
+  try{
+    const response =  await fetch(rebrandlyEndpoint, {
+      method: 'POST',
       body: data,
       headers: {
-        'Content-type': 'application/json',
-				'apikey': apiKey
+        "Content-type": "application/json",
+        'apikey': apiKey
       }
-    });
-		if(response.ok){
+    })
+    if(response.ok){
       const jsonResponse = await response.json();
-      renderResponse(jsonResponse);
+			renderByteResponse(jsonResponse);
     }
-  } catch (error) {
+  }
+  catch(error){
     console.log(error);
   }
 }
