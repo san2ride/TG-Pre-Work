339e280488540b8d93443d2e5462bdeba366d908
san2ride <sanchezjason@me.com>
2018-08-06 01:46:01 -0600

    Loops

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-// Review: Iterators
-let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
+let cookies = ['chocolate chip', 'raisin', 'macadamia nut', 'sugar'];
 
-let nums = [1, 50, 75, 200, 350, 525, 1000];
-
-//  Choose a method that will return undefined
-cities.forEach(city => console.log('Have you visited ' + city + '?'));
-
-// Choose a method that will return a new array
-let longCities = cities.filter(city => city.length > 7);
-
-// Choose a method that will return a new array
-let smallerNums = nums.map(num => num - 5);
-
-// Choose a method that will return a boolean value
-nums.every(num => num < 0);
+for (let i = 0; i<cookies.length; i++) {
+  console.log('I would love to eat a ' + cookies[i] + ' cookie right now!');
+}
