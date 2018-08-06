6f8af155f688657012b528569c6cc6c96493313e
san2ride <sanchezjason@me.com>
2018-08-05 20:05:38 -0600

    Functions

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-let calculatorIsOn = false;
+const takeOrder = () => {
+  console.log("Order: pizza")
+}
 
-const pressPowerButton = () => {
-  if (calculatorIsOn) {
-    console.log('Calculator turning off.');
-    calculatorIsOn = false;
-  } else {
-    console.log('Calculator turning on.');
-    calculatorIsOn = true;
-  }
-};
-
-pressPowerButton();
-// Output: Calculator turning on.
-
-pressPowerButton();
-// Output: Calculator turning off.
+takeOrder()
