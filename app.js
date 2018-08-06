ff738ebb59a91061f1d6427eb03df42550d3b210
san2ride <sanchezjason@me.com>
2018-08-06 02:21:41 -0600

    The this Keyword II

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ let person = {
   weekAlarm: 'Alarm set to 7AM',
 
   sayHello: function() {
-    return `Hello, my name is ${person.name}`;
+    return `Hello, my name is ${this.name}`;
   },
 
   sayGoodbye() {
     return 'Goodbye!';
   }
-
 };
 
+
+let friend = {
+  name: 'Lebron'
+};
+
+friend.sayHello = person.sayHello;
+
+console.log(friend.sayHello());
 console.log(person.sayHello());
 
 person.hobbies = ['Basketball', 'Coaching'];
