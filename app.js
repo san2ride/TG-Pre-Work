fb738ecea4eb6aacf95c5ef2274cdd8fc29695cf
san2ride <sanchezjason@me.com>
2018-08-06 02:52:50 -0600

    Transpilation With Babel

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var pasta = "Spaghetti"; // ES5 syntax
 
-var meat = "Pancetta"; // ES6 syntax
+const meat = "Pancetta"; // ES6 syntax
 
-var sauce = "Eggs and cheese"; // ES6 syntax
+let sauce = "Eggs and cheese"; // ES6 syntax
 
 // Template literals, like the one below, were introduced in ES6
-var carbonara = "You can make carbonara with " + pasta + ", " + meat + ", " + " and a sauce made with " + sauce + ".";
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
