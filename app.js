295900c557b4114190ee08d55d1655d9fa252381
san2ride <sanchezjason@me.com>
2018-08-06 03:32:23 -0600

    Review Requests I

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
-// Information to reach API
-const apiKey = '<Your API Key>';
-const url = 'https://api.rebrandly.com/v1/links';
+// NOTE: wordSmith functions from lines 4 - 39
+// NOTE: byteSize functions from lines 41 - 76 (remember to add your API key!)
 
-// Some page elements
+// information to reach API
+const dataMuseUrl = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jjb=';
+
+// selecting page elements
 const inputField = document.querySelector('#input');
-const shortenButton = document.querySelector('#shorten');
+const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+  const wordQuery = inputField.value;
+  const endPoint = dataMuseUrl + queryParams + wordQuery;
+
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      renderWordResponse(xhr.response);
+    }
+  };
+  xhr.open('GET', endPoint);
+  xhr.send();
+}
+
+// clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  };
+  getSuggestions();
+};
+
+submit.addEventListener('click', displaySuggestions);
+
+// information to reach Rebrandly API
+const apiKey = '<Your API Key>';
+const rebrandlyUrl = 'https://api.rebrandly.com/v1/links';
+
+// element selector
+const shortenButton = document.querySelector('#shorten');
 
 // AJAX functions
 const shortenUrl = () => {
@@ -17,23 +55,22 @@ const shortenUrl = () => {
 
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
-  		renderResponse(xhr.response);
-		}
-  }
-  xhr.open('POST', url);
+      renderByteResponse(xhr.response);
+    }
+  };
+  xhr.open('POST', rebrandlyUrl);
   xhr.setRequestHeader('Content-type', 'application/json');
 	xhr.setRequestHeader('apikey', apiKey);
   xhr.send(data);
 }
-
 
 // Clear page and call AJAX functions
 const displayShortUrl = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
     responseField.removeChild(responseField.firstChild);
-  }
+  };
   shortenUrl();
-}
+};
 
 shortenButton.addEventListener('click', displayShortUrl);
