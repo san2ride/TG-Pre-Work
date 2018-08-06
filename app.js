12344a6b0f1b78a691bf983c1fd428f1136157cf
san2ride <sanchezjason@me.com>
2018-08-06 03:21:26 -0600

    Combining Import Statements

--- app.js
+++ app.js
@@ -1,38 +1,27 @@
-export let availableAirplanes = [
-{name: 'AeroJet',
- fuelCapacity: 800,
- availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
- maxSpeed: 1200,
- minSpeed: 300
-},
-{name: 'SkyJet',
- fuelCapacity: 500,
- availableStaff: ['pilots', 'flightAttendants'],
- maxSpeed: 800,
- minSpeed: 200
+import { availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+
+import meetsSpeedRangeRequirements from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element['fuelCapacity']);
+  });
 }
-];
 
-export let flightRequirements = {
-  requiredStaff: 4,
-  requiredSpeedRange: 700
-};
+displayFuelCapacity();
 
-export function meetsStaffRequirements(availableStaff, requiredStaff) {
-  if (availableStaff.length >= requiredStaff) {
-    return true;
-  } else {
-    return false;
-  }
-};
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
 
-export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
-  let range = maxSpeed - minSpeed;
-  if (range > requiredSpeedRange) {
-    return true;
-    } else {
-    return false;
-  }
-};
+displayStaffStatus();
 
-export default meetsSpeedRangeRequirements;
+function displaySpeedRangeStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets speed range requirements:' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+  });
+}
+
+displaySpeedRangeStatus();
