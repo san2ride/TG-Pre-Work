0a58d2df05942a366f5d9f9dd4f1f7d8e143d1af
san2ride <sanchezjason@me.com>
2018-08-05 22:36:39 -0600

    length property

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Update Elements
 let newYearsResolutions = ['Run a marathon', 'Learn a new language', 'Read 52 books'];
 
 let listItem = newYearsResolutions[0];
@@ -6,3 +5,4 @@ console.log(listItem);
 
 console.log(newYearsResolutions[2]);
 console.log(newYearsResolutions[3]);
+console.log(newYearsResolutions.length)
