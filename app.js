a19b21245c4ffa78dddf2b1e77a0e8aedac44243
san2ride <sanchezjason@me.com>
2018-08-06 03:33:08 -0600

    Introduction to Requests with ES6

--- app.js
+++ app.js
@@ -1,76 +1,50 @@
-// NOTE: wordSmith functions from lines 4 - 39
-// NOTE: byteSize functions from lines 41 - 76 (remember to add your API key!)
+const jsonButton = document.querySelector('#generate');
+const buttonContainer = document.querySelector('#buttonContainer');
+const display = document.querySelector('#displayContainer');
+const collection = ["Another", "More", "Next", "Continue", "Keep going", "Click me", "A new one"];
 
-// information to reach API
-const dataMuseUrl = 'https://api.datamuse.com/words?';
-const queryParams = 'rel_jjb=';
-
-// selecting page elements
-const inputField = document.querySelector('#input');
-const submit = document.querySelector('#submit');
-const responseField = document.querySelector('#responseField');
-
-// AJAX function
-const getSuggestions = () => {
-  const wordQuery = inputField.value;
-  const endPoint = dataMuseUrl + queryParams + wordQuery;
-
-  const xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      renderWordResponse(xhr.response);
+const generateJson = async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if(response.ok){
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+      changeButton();
     }
-  };
-  xhr.open('GET', endPoint);
-  xhr.send();
-}
-
-// clear previous results and display results to webpage
-const displaySuggestions = (event) => {
-  event.preventDefault();
-  while(responseField.firstChild){
-    responseField.removeChild(responseField.firstChild);
-  };
-  getSuggestions();
+  } catch(error) {
+    console.log(error);
+  }
 };
 
-submit.addEventListener('click', displaySuggestions);
-
-// information to reach Rebrandly API
-const apiKey = '<Your API Key>';
-const rebrandlyUrl = 'https://api.rebrandly.com/v1/links';
-
-// element selector
-const shortenButton = document.querySelector('#shorten');
-
-// AJAX functions
-const shortenUrl = () => {
-  const urlToShorten = inputField.value;
-  const data = JSON.stringify({destination: urlToShorten});
-
-  const xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      renderByteResponse(xhr.response);
+const formatJson = (resJson) => {
+  resJson = JSON.stringify(resJson);
+  let counter = 0;
+  return resJson.split('')
+  .map(char => {
+    switch (char) {
+      case ',':
+        return `,\n${' '.repeat(counter * 2)}`;
+      case '{':
+        counter += 1;
+        return `{\n${' '.repeat(counter * 2)}`;
+      case '}':
+        counter -= 1;
+        return `\n${' '.repeat(counter * 2)}}`;
+      default:
+        return char;
     }
-  };
-  xhr.open('POST', rebrandlyUrl);
-  xhr.setRequestHeader('Content-type', 'application/json');
-	xhr.setRequestHeader('apikey', apiKey);
-  xhr.send(data);
-}
-
-// Clear page and call AJAX functions
-const displayShortUrl = (event) => {
-  event.preventDefault();
-  while(responseField.firstChild){
-    responseField.removeChild(responseField.firstChild);
-  };
-  shortenUrl();
+  })
+  .join('');
 };
 
-shortenButton.addEventListener('click', displayShortUrl);
+const renderResponse = (jsonResponse) => {
+  const jsonSelection = Math.floor(Math.random() * 10);
+  display.innerHTML = `<pre>${formatJson(jsonResponse[jsonSelection])}</pre>`;
+};
+
+const changeButton = () => {
+  const newText = Math.floor(Math.random() * 7);
+  jsonButton.innerHTML = `${collection[newText]}!`;
+};
+
+jsonButton.addEventListener('click', generateJson);
