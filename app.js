b7e69fbf9a2c5bef3ddf01f4c988a690ee950f39
san2ride <sanchezjason@me.com>
2018-08-06 02:04:00 -0600

    Objects

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
-
-groceryList.shift();
-
-groceryList.unshift('popcorn');
-console.log(groceryList);
-
-console.log(groceryList.slice(1, 4));
-console.log(groceryList);
+let person = {
+  name: 'Jason',
+  age: 40
+};
