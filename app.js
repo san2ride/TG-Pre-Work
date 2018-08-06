dafa775f4ac68b2110be156406c2a03d95a7b082
san2ride <sanchezjason@me.com>
2018-08-06 02:24:22 -0600

    Getters and Setters I

--- app.js
+++ app.js
@@ -1,43 +1,15 @@
 let person = {
-  name: 'Tyron',
-  age: 40,
-  weekendAlarm: 'No alarms needed',
-  weekAlarm: 'Alarm set to 7AM',
+  _name: 'Lu Xun',
+  _age: 137,
 
-  sayHello: function() {
-    return `Hello, my name is ${this.name}`;
-  },
-
-  sayGoodbye() {
-    return 'Goodbye!';
+  set age(ageIn) {
+    if (typeof ageIn === 'number') {
+      this._age = ageIn;
+    }
+    else {
+      console.log('Invalid input');
+      return 'Invalid input';
+    }
   }
+
 };
-
-
-let friend = {
-  name: 'Lebron'
-};
-
-friend.sayHello = person.sayHello;
-
-console.log(friend.sayHello());
-console.log(person.sayHello());
-
-person.hobbies = ['Basketball', 'Coaching'];
-person.hobbies = ['Basketball'];
-console.log(person.hobbies);
-
-
-console.log(person['name']);
-console.log(person['age']);
-
-let day = 'Tuesday';
-let alarm;
-
-if (day === 'Saturday' || day === 'Sunday' ) {
-  alarm = 'weekendAlarm';
-} else {
-  alarm = 'weekAlarm';
-}
-
-console.log(person[alarm]);
