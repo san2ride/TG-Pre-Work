a10a62b205a037c72c4240bb7f12da715637317f
san2ride <sanchezjason@me.com>
2018-08-05 22:21:15 -0600

    Block Scope II

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const visibleLightWaves = () => {
-  let lightWaves = 'Moonlight'
-  console.log(lightWaves)
-}
+  let lightWaves = 'Moonlight';
+  let region = 'The Arctic';
+  if (region === 'The Arctic') {
+    let lightWaves = 'Northern Lights';
+    console.log(lightWaves);
+  }
+  console.log(lightWaves);
+};
 
-visibleLightWaves()
-console.log(lightWaves)
+visibleLightWaves();
