50d560850a9b49708b96108284415042d295cb99
san2ride <sanchezjason@me.com>
2018-08-06 02:11:57 -0600

    Adding a Property

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ let person = {
   name: 'Tyron',
   age: 40,
   weekendAlarm: 'No alarms needed',
-  weekAlarm: 'Alarm set to 7AM'
+  weekAlarm: 'Alarm set to 7AM',
+  hobbies: ["swimming", "biking", "skiing"]
 };
 
 console.log(person['name']);
 console.log(person['age']);
+console.log(person.hobbies)
 
 let day = 'Tuesday';
 let alarm;
